77f0f63461c4947e0ee6d039ac85da8d80bb7c2f
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-02 22:25:07 +0530

    Update login.dart

--- app/lib/login.dart
+++ app/lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class login extends StatefulWidget {
   const login({Key? key}) : super(key: key);
 
@@ -13,89 +14,109 @@ class _loginState extends State<login> {
       backgroundColor: Colors.yellow[800],
       body: SafeArea(
         child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.abc_sharp),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Image(
+                  image: AssetImage(
+                    'assets/12.png',
+                  ),
+                  height: 300,
+                ),
+              ),
 
-                //email
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Email',
-                        ),
+              //email
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Email',
                       ),
                     ),
                   ),
                 ),
+              ),
 
-                SizedBox(
-                  height: 20,
-                ),
+              SizedBox(
+                height: 20,
+              ),
 
-                //password
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Password',
-                        ),
+              //password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Password',
                       ),
                     ),
                   ),
                 ),
+              ),
 
-                SizedBox(
-                  height: 20,
-                ),
+              SizedBox(
+                height: 20,
+              ),
 
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: OutlinedButton(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.black,
                     ),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(
-                        width: 2.0, // set the border weight to 2.0
-                        color: Colors.green,
-                      ),
-                      fixedSize: Size(150, 50),
-                    ),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => login()));
-                    },
                   ),
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(
+                      width: 2.0, // set the border weight to 2.0
+                      color: Colors.green,
+                    ),
+                    fixedSize: Size(150, 50),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => login()));
+                  },
                 ),
+              ),
 
-                SizedBox(
-                  height: 20,
-                ),
+              SizedBox(
+                height: 20,
+              ),
 
-              ],
-            ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member? .'),
+                  Text("Not a member? .",style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                ],
+              ),
+
+              
+            ],
+          ),
         ),
       ),
     );
