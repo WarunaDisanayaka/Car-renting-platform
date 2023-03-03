cd9b0562ddaa43d5c0d0668caa8ce9be825c4f86
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-03 22:31:33 +0530

    Update home.dart

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -10,6 +10,45 @@ class home extends StatefulWidget {
 class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Select'),
+      ),
+      backgroundColor: Colors.amber,
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 30,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(1)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          // handle the tap event
+                          print('Icon tapped!');
+                        },
+                      ),
+                      hintText: 'Email',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+
+        ),
+      ),
+    );
   }
 }
