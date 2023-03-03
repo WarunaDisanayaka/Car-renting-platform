5f3b61b39726df6c66f9f685c7d57a117958d335
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-04 00:58:59 +0530

    Update home.dart

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -7,8 +7,8 @@ class home extends StatefulWidget {
   @override
   State<home> createState() => _homeState();
 }
-
-TextEditingController _controller = TextEditingController(text: "2022/03/05");
+String txt ="2023/03/03";
+TextEditingController _controller = TextEditingController(text: txt);
 
 class _homeState extends State<home> {
   @override
@@ -21,12 +21,21 @@ class _homeState extends State<home> {
       backgroundColor: Colors.amber,
       body: Container(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              height: 30,
-            ),
 
             //select stsrt date
+            Padding(
+            padding: EdgeInsets.only(top: 20, bottom: 8, left: 25, right: 5),
+              child: Text(
+                "Select Pic-Up Date ",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ),
+
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: Container(
@@ -84,6 +93,80 @@ class _homeState extends State<home> {
                 ),
               ),
             ),
+
+
+
+            //select END date
+            Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 8, left: 25, right: 5),
+              child: Text(
+                "Select END Date ",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(3)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          // handle the tap event
+                          print('Icon tapped!');
+                          showCupertinoModalPopup(
+                            context: context,
+                            builder: (context) {
+                              return Container(
+                                height:
+                                MediaQuery.of(context).size.height * 0.36,
+                                decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(40.0),
+                                    topRight: Radius.circular(40.0),
+                                  ),
+                                ),
+                                child: Column(
+                                  children: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text('Done'),
+                                    ),
+                                    Expanded(
+                                      child: CupertinoDatePicker(
+                                        onDateTimeChanged: (date) {},
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                      hintText: '2023/03/20',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+
           ],
         ),
       ),
