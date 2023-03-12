d7191391685532767ed0f2b2d6217a42229c4cd8
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-12 18:58:32 +0530

    Update viewvehicle.dart

--- app/lib/viewvehicle.dart
+++ app/lib/viewvehicle.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
 class view extends StatefulWidget {
   const view({Key? key}) : super(key: key);
 
@@ -10,10 +12,30 @@ class _viewState extends State<view> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Select'),
+      ),
       body: Container(
         child: Column(
           children: [
+            // image CarouselSlider
+            CarouselSlider(
+              items: imageList.map((imageUrl) {
+                return Image.network(
+                  imageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                );
+              }).toList(),
+              options: CarouselOptions(
+                autoPlay: true,
+                aspectRatio: 16/9,
+                enlargeCenterPage: true,
+                viewportFraction: 0.8,
+              ),
+            )
+
 
           ],
         ),
