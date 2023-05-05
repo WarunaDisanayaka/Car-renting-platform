a89eb3c72d2877035b8c1220edade87417b288e7
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-05-05 23:11:57 +0530

    work

--- app/lib/viewvehicle.dart
+++ app/lib/viewvehicle.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class view extends StatefulWidget {
@@ -16,7 +18,7 @@ class _ViewState extends State<view> {
       backgroundColor: Color.fromRGBO(47, 114, 100, 1),
       extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color.fromRGBO(47, 114, 100, 1),
         elevation: 0.0,
       ),
       drawer: Drawer(
@@ -53,61 +55,149 @@ class _ViewState extends State<view> {
         ),
       ),
 
-      body: Center(
-        child: Column(
-          children: [
-
-            Image(
-              image: AssetImage(
-                'assets/cr4.jpeg',
-              ),
-            ),
-
-
-            Text(
-              widget.product["model"] ?? "",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              "RS: ${widget.product["price"] ?? ""}  | Per Day",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              "Fuel Type: ${widget.product["fuelType"] ?? ""}",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              "Seats: ${widget.product["seats"] ?? ""}",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              "Transmission: ${widget.product["transmission"] ?? ""}",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-          ],
-        ),
+      body: Stack(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Image.asset('assets/cr4.jpeg'),
+            height: 390,
+          ),
+          scroll(),
+        ],
       ),
     );
   }
+
+
+  scroll() {
+    return DraggableScrollableSheet(
+        initialChildSize: 0.6,
+        maxChildSize: 1.0,
+        minChildSize: 0.6,
+        builder: (context, scrollController) {
+          return Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            clipBehavior: Clip.hardEdge,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(20),
+                  topRight: const Radius.circular(20)),
+            ),
+            child: SingleChildScrollView(
+              controller: scrollController,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 5,
+                          width: 35,
+                          color: Colors.black12,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text("data"),
+                  Text(
+                    widget.product["model"] ?? "",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "RS: ${widget.product["price"] ?? ""}  | Per Day",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "Fuel Type: ${widget.product["fuelType"] ?? ""}",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Seats: ${widget.product["seats"] ?? ""}",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Transmission: ${widget.product["transmission"] ?? ""}",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+
+  steps(BuildContext context, int index) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.green,
+            radius: 12,
+            child: Text("${index + 1}"),
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: 270,
+                child: Text(
+                  "Your recipe has been uploaded, you can see it on your profile. Your recipe has been uploaded, you can see it on your",
+                  maxLines: 3,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodyText2!
+                      .copyWith(color: Colors.green),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Image.asset(
+                "assets/imges/Rectangle 219.png",
+                height: 155,
+                width: 270,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
 }
+
+
+
+
+
+
 
 
