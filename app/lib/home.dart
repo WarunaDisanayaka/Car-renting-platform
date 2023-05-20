008c491ec679766a1ba7c1db1ebaf213dfb4d6df
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-05-21 00:14:19 +0530

    paypal

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:math';
 import 'package:cr_app/myaccount.dart';
+import 'package:cr_app/paypal.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
 
@@ -377,60 +378,127 @@ class _HomeState extends State<home> {
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-                child: Center(
-                  child: GestureDetector(
-                    onTap: () async {
-                      final user = FirebaseAuth.instance.currentUser;
-                      final address = _locationController.text;
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: () async {
+                          final user = FirebaseAuth.instance.currentUser;
+                          final address = _locationController.text;
 
-                      if (address.isEmpty || selectedDateTime.isEmpty || selectedDateTime2.isEmpty) {
-                        // Show error message here (e.g., using a dialog)
-                        showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                            title: Text('Error'),
-                            content: Text('Fill all.'),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: Text('OK'),
+                          if (address.isEmpty || selectedDateTime.isEmpty || selectedDateTime2.isEmpty) {
+                            // Show error message here (e.g., using a dialog)
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: Text('Error'),
+                                content: Text('Fill all.'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: Text('OK'),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        );
-                      } else {
-                        final ordersRef = FirebaseFirestore.instance.collection('orders');
-                        final newOrder = await ordersRef.add({
-                          'dateTime': selectedDateTime,
-                          'dateTime2': selectedDateTime2,
-                          'productModel': "${widget.product["model"] ?? ""}",
-                          'user': user?.uid,
-                          'address': address,
-                        });
+                            );
+                          } else {
+                            final ordersRef = FirebaseFirestore.instance.collection('orders');
+                            final newOrder = await ordersRef.add({
+                              'dateTime': selectedDateTime,
+                              'dateTime2': selectedDateTime2,
+                              'productModel': "${widget.product["model"] ?? ""}",
+                              'user': user?.uid,
+                              'address': address,
+                            });
 
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => MyAccount()),
-                        );
-                      }
-                    },
-                    child: Container(
-                      color: Color.fromRGBO(47, 114, 100, 1),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(60, 15, 63, 15),
-                        child: Text(
-                          "Order",
-                          style: TextStyle(
-                            color: Colors.white,
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => MyAccount()),
+                            );
+                          }
+                        },
+                        child: Container(
+                          color: Color.fromRGBO(47, 114, 100, 1),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(60, 15, 63, 15),
+                            child: Text(
+                              "Order",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
+
+                  SizedBox(width: 10,),
+
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: () async {
+                          final user = FirebaseAuth.instance.currentUser;
+                          final address = _locationController.text;
+
+                          if (address.isEmpty || selectedDateTime.isEmpty || selectedDateTime2.isEmpty) {
+                            // Show error message here (e.g., using a dialog)
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: Text('Error'),
+                                content: Text('Fill all.'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: Text('OK'),
+                                  ),
+                                ],
+                              ),
+                            );
+                          } else {
+                            final ordersRef = FirebaseFirestore.instance.collection('orders');
+                            final newOrder = await ordersRef.add({
+                              'dateTime': selectedDateTime,
+                              'dateTime2': selectedDateTime2,
+                              'productModel': "${widget.product["model"] ?? ""}",
+                              'user': user?.uid,
+                              'address': address,
+                            });
+
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => paypal(title: '',)),
+                              // MaterialPageRoute(builder: (context) => MyAccount()),
+                            );
+                          }
+                        },
+                        child: Container(
+                          color: Color.fromRGBO(0, 48, 135, 1),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(60, 15, 63, 15),
+                            child: Text(
+                              "PayPal",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
+
+
+
+
 
               //CarouselSlider
 
--- app/lib/paypal.dart
+++ app/lib/paypal.dart
@@ -1,3 +1,169 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cr_app/myaccount.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_paypal/flutter_paypal.dart';
 
+class Paypal extends StatelessWidget {
+  const Paypal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Paypal',
+      theme: ThemeData(),
+      home: const paypal(title: 'PayPal'),
+    );
+  }
+}
+
+class paypal extends StatefulWidget {
+  const paypal({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<paypal> createState() => _paypalState();
+}
+
+class _paypalState extends State<paypal> {
+  final databaseReference = FirebaseFirestore.instance;
+
+  List<Map<dynamic, dynamic>> productList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchProducts();
+  }
+
+  Future<void> fetchProducts() async {
+    final QuerySnapshot snapshot =
+    await databaseReference.collection("products").get();
+    setState(() {
+      productList = snapshot.docs
+          .map((DocumentSnapshot document) => document.data() as Map<dynamic, dynamic>)
+          .toList();
+    });
+  }
+
+  void redirectToMyAccount() {
+    // Perform the redirection to myaccount() here
+    // Replace this code with your actual redirection logic
+    MyAccount();
+    print('Redirecting to myaccount()');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(47, 114, 100, 1),
+        title: Text(widget.title),
+      ),
+      body: productList.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+        children: [
+          SizedBox(
+            height: 100,
+          ),
+          Icon(
+            Icons.paypal,
+            size: 120,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "TOTAL| RS: ${productList[0]["price"] ?? ""}",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => UsePaypal(
+                      sandboxMode: true,
+                      clientId:
+                      "AYVv_a07kA5gci4uV4KVMWthAu-is9z2tfWVtdomEhUZOj62SUzMeqlwC-m97CaBZvc8Se6ifliKQtBS",
+                      secretKey:
+                      "ENKPY5NucYYlfDLKJhMiGnCBlKbUuhFz4QCn7EMvEGeO5DoryTFsSHct708N3qz5ysHlJzd5Qox5jUHd",
+                      returnURL: "https://samplesite.com/return",
+                      cancelURL: "https://samplesite.com/cancel",
+                      transactions: const [
+                        {
+                          "amount": {
+                            "total": '10.12',
+                            "currency": "USD",
+                            "details": {
+                              "subtotal": '10.12',
+                              "shipping": '0',
+                              "shipping_discount": 0
+                            }
+                          },
+                          "description":
+                          "The payment transaction description.",
+                          "item_list": {
+                            "items": [
+                              {
+                                "name": "A demo product",
+                                "quantity": 1,
+                                "price": '10.12',
+                                "currency": "USD"
+                              }
+                            ],
+                            "shipping_address": {
+                              "recipient_name": "Jane Foster",
+                              "line1": "Travis County",
+                              "line2": "",
+                              "city": "Austin",
+                              "country_code": "US",
+                              "postal_code": "73301",
+                              "phone": "+00000000",
+                              "state": "Texas"
+                            },
+                          }
+                        }
+                      ],
+                      note: "Contact us for any questions on your order.",
+                      onSuccess: (Map params) async {
+                        print("onSuccess: $params");
+                        redirectToMyAccount(); // Redirect after successful payment
+                      },
+                      onError: (error) {
+                        print("onError: $error");
+                      },
+                      onCancel: (params) {
+                        print('cancelled: $params');
+                      },
+                    ),
+                  ),
+                );
+              },
+              child: Container(
+                color: Color.fromRGBO(47, 114, 100, 1),
+                height: 50,
+                width: 120,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Text(
+                      "Make Payment",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
